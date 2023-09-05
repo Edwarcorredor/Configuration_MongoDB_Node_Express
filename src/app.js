1d829d56a4f6d19c8a4607cd2970074002cb4034
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-05 09:16:46 -0500

    feat: NUEVAS CONFIGURACIONES

--- src/app.js
+++ src/app.js
@@ -5,13 +5,30 @@ import loginRouter from "./routes/loginRouter.js";
 
 dotenv.config();
 
+
+acl.config({
+  filename: 'nacl.json',
+  baseUrl: 'api',
+  roleSearchPath: 'user.rol',
+  denyCallback: (res) => res.status(403).json({
+    message: 'No tienes permisos para acceder a este recurso'
+  })
+});
+
 const app = express();
 app.use(express.json());
 
 app.use('/auth', loginRouter)
-app.use(passportConfig.initialize())
-app.use(passportConfig.authenticate('bearer', { session: false }))
+app.use(passportHelper.initialize());
 
+app.get('/token/:usuario', crearToken);
+
+app.get('/api/admin', passportHelper.authenticate('bearer', { session: false }), acl.authorize, (req, res) => {
+  res.json({mensaje: 'Hola admin', usuario: req.user});
+});
+app.get('/api/vendedor', passportHelper.authenticate('bearer', { session: false }), acl.authorize, (req, res) => {
+  res.json({mensaje: 'Hola vendedor', usuario: req.user});
+});
 
 
 let config = JSON.parse(process.env.MY_SERVER);
